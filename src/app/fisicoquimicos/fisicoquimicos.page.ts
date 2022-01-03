c443e47c9ed84a6db9bc3abc2f94f3c50b92022e
unknown <Administrador@colaca.local>
2022-01-03 14:10:19 -0400

    con limpiardata

--- src/app/fisicoquimicos/fisicoquimicos.page.ts
+++ src/app/fisicoquimicos/fisicoquimicos.page.ts
@@ -105,7 +105,7 @@ export class FisicoquimicosPage implements OnInit {
     })
   }
   limpiarData(){
-    this.DataForm=={
+    this.DataForm={
       observaciones:'',
       fecha:new Date().toString(),
       startdate:new Date().toString(),
