338dc71af62d707f0f5a4e8df2ea95fe487cedd6
piscitecapi$piscitecapi <lpr@myexobuy.com>
2022-10-03 01:11:29 -0400

    Agregado

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -22,11 +22,14 @@ export class HomePage {
     name:"Fisicos - Quimicos"
   }]
   inventarios=[{
-    id:1,
+    id:5,
     name:"Consumo de Materias"
   }]
   valorinicial=0
-  constructor(private navcontorll:NavController,private master:MasterService) {}
+  constructor(private navcontorll:NavController,private master:MasterService) {
+
+
+  }
   radioGroupChange(evento){
     this.valorinicial=evento.detail.value;
   }
@@ -35,11 +38,115 @@ export class HomePage {
 
     }else{
       if(this.valorinicial==1){
-        this.navcontorll.navigateForward("menu/reportinicial")
+        this.master.storage.getItems(this.master.storage.arrayname.Permisos).then((datos)=>{
+          this.master.storage.getItems(this.master.storage.arrayname.UsuarioActivo).then((UsusarioActiv)=>{
+          if(datos.length>0){
+            let valorPaso=false
+            datos[0].forEach((valor)=>{
+              if(String(valor.UsuarioAcc)==String(UsusarioActiv[0]['Cedula'])){
+                if(valor.IdMod==2 && valor.IdHoja==12){
+                  if(valor.PermisoIng==1){
+                    valorPaso=true;
+                  }
+                }
+              }
+            })
+            if(valorPaso){
+              this.navcontorll.navigateForward("menu/reportinicial")
+            }else{
+              this.master.toastMensaje("No tienes Permiso para ingresar",2000)
+            }
+          }
+        })
+        })
       }else  if(this.valorinicial==2){
-        this.navcontorll.navigateForward("menu/reportvacun")
-      }else{
-        this.navcontorll.navigateForward("menu/fisicoquimicos")
+        this.master.storage.getItems(this.master.storage.arrayname.Permisos).then((datos)=>{
+          this.master.storage.getItems(this.master.storage.arrayname.UsuarioActivo).then((UsusarioActiv)=>{
+          if(datos.length>0){
+            let valorPaso=false
+            datos[0].forEach((valor)=>{
+              if(String(valor.UsuarioAcc)==String(UsusarioActiv[0]['Cedula'])){
+                if(valor.IdMod==2 && valor.IdHoja==14){
+                  if(valor.PermisoIng==1){
+                    valorPaso=true;
+                  }
+                }
+              }
+            })
+            if(valorPaso){
+              this.navcontorll.navigateForward("menu/reportvacun")
+            }else{
+              this.master.toastMensaje("No tienes Permiso para ingresar",2000)
+            }
+          }
+        })
+        })
+      }else if(this.valorinicial==3){
+        this.master.storage.getItems(this.master.storage.arrayname.Permisos).then((datos)=>{
+          this.master.storage.getItems(this.master.storage.arrayname.UsuarioActivo).then((UsusarioActiv)=>{
+          if(datos.length>0){
+            let valorPaso=false
+            datos[0].forEach((valor)=>{
+              if(String(valor.UsuarioAcc)==String(UsusarioActiv[0]['Cedula'])){
+                if(valor.IdMod==2 && valor.IdHoja==10){
+                  if(valor.PermisoIng==1){
+                    valorPaso=true;
+                  }
+                }
+              }
+            })
+            if(valorPaso){
+              this.navcontorll.navigateForward("menu/fisicoquimicos")
+            }else{
+              this.master.toastMensaje("No tienes Permiso para ingresar",2000)
+            }
+          }
+        })
+        })
+      }else if(this.valorinicial==4){
+        this.master.storage.getItems(this.master.storage.arrayname.Permisos).then((datos)=>{
+          this.master.storage.getItems(this.master.storage.arrayname.UsuarioActivo).then((UsusarioActiv)=>{
+          if(datos.length>0){
+            let valorPaso=false
+            datos[0].forEach((valor)=>{
+              if(String(valor.UsuarioAcc)==String(UsusarioActiv[0]['Cedula'])){
+                if(valor.IdMod==4 && valor.IdHoja==1){
+                  if(valor.PermisoIng==1){
+                    valorPaso=true;
+                  }
+                }
+              }
+            })
+            if(valorPaso){
+              this.navcontorll.navigateForward("menu/mortalidad")
+            }else{
+              this.master.toastMensaje("No tienes Permiso para ingresar",2000)
+            }
+          }
+        })
+        })
+      }else if(this.valorinicial==5){
+        this.master.storage.getItems(this.master.storage.arrayname.Permisos).then((datos)=>{
+          this.master.storage.getItems(this.master.storage.arrayname.UsuarioActivo).then((UsusarioActiv)=>{
+          if(datos.length>0){
+            let valorPaso=false
+            datos[0].forEach((valor)=>{
+              if(String(valor.UsuarioAcc)==String(UsusarioActiv[0]['Cedula'])){
+                if(valor.IdMod==4 && valor.IdHoja==1){
+                  if(valor.PermisoIng==1){
+                    valorPaso=true;
+                  }
+                }
+              }
+            })
+            if(valorPaso){
+              this.navcontorll.navigateForward("menu/materiasconsumo")
+            }else{
+              this.master.toastMensaje("No tienes Permiso para ingresar",2000)
+            }
+          }
+        })
+        })
       }
     }
   }
